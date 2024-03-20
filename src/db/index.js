03bec501307366c318cd5550a30b7cbc0bb44061
Kevin Chen <kchen1025@gmail.com>
2024-03-20 10:16:58 -0700

    certs for postgres

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,22 @@
 const { Pool } = require("pg");
+const fs = require("fs");
+
+let ssl = {};
+if (process.env.NODE_ENV === "production") {
+  const cert = Buffer.from(process.env.CERTIFICATE_BASE64, "base64").toString();
+  ssl = {
+    rejectUnauthorized: true,
+    ca: cert, // Use the decoded certificate here
+  };
+} else {
+  ssl = {
+    ca: fs.readFileSync("//usr/local/var/postgresql@14/server.crt").toString(),
+  };
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: process.env.NODE_ENV !== "production" ? false : true,
-  ssl: false,
+  ssl: ssl,
 });
 
 module.exports = {
