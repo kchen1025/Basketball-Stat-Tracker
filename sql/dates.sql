f0bf0deebe879a2f8d3e83887e6186d2377e432c
Kevin Chen <kchen1025@gmail.com>
2024-03-27 13:33:58 -0700

    save all dates

--- sql/dates.sql
+++ sql/dates.sql
@@ -1,3 +1,69 @@
 
+update game set date='3/18/2023' where name like 'D1G%';
+
+update game set date='3/25/2023' where name like 'D2G%';
+
+update game set date='4/1/2023' where name like 'D3G%';
+
+update game set date='4/8/2023' where name like 'D4G%';
+
+update game set date='4/22/2023' where name like 'D5G%';
+
+update game set date='4/29/2023' where name like 'D6G%';
+
+update game set date='5/06/2023' where name like 'D7G%';
+
+update game set date='5/13/2023' where name like 'D8G%';
+
+update game set date='5/27/2023' where name like 'D9G%';
+
+update game set date='6/3/2023' where name like 'D10G%';
+
+update game set date='6/10/2023' where name like 'D11G%';
+
+update game set date='6/24/2023' where name like 'D12G%';
+
+update game set date='7/8/2023' where name like 'D13G%';
+
+update game set date='7/15/2023' where name like 'D14G%';
+
+update game set date='7/23/2023' where name like 'D15G%';
+
+update game set date='7/29/2023' where name like 'D16G%';
+
+update game set date='8/12/2023' where name like 'D17G%';
+
+update game set date='8/19/2023' where name like 'D18G%';
+
+update game set date='8/26/2023' where name like 'D19G%'; -- this is uncertain 
 
 update game set date='10/14/2023' where name like 'D20G%';
+
+update game set date='10/21/2023' where name like 'D21G%';
+
+update game set date='12/2/2023' where name like 'D22G%';
+
+update game set date='12/16/2023' where name like 'D23G%';
+
+update game set date='12/23/2023' where name like 'D24G%';
+
+update game set date='1/6/2024' where name like 'D25G%';
+
+update game set date='1/13/2024' where name like 'D26G%';
+
+update game set date='1/27/2024' where name like 'D27G%';
+
+update game set date='2/3/2024' where name like 'D28G%';
+
+update game set date='2/24/2024' where name like 'D30G%';
+
+update game set date='3/9/2024' where name like 'D31G%';
+
+update game set date='3/16/2024' where name like 'D32G%';
+
+
+
+
+
+
+
